b4147485838b3fe57f01c69beb9cd363f480bc7c
roseQuartzUnderTheMoon <alexis.olivo.alvarez@gmail.com>
2021-12-13 21:06:17 -0600

    Main Branch Completed
    Main/Most Favorable Solution to the challenge.

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 document.addEventListener("DOMContentLoaded", allWorkAndNoPlayisNoFunAtAll, false);
 
-var myMessage = ['H', 'e', 'l', 'l', 'o', 'W', 'o', 'r', 'l', 'd'];
-var i = 0, j = 0, mySound;
+var myMessage = ['H', 'e', 'l', 'l', 'o', ' ','W', 'o', 'r', 'l', 'd']; //mentioned in commentary 1 in README.md
+var i = 0, mySound, text;
 
 function allWorkAndNoPlayisNoFunAtAll() {
 	console.log("Get to Play!");
 
 	mySound = document.getElementById("bling");
+	text = document.getElementById("hello_world"); //mentioned in commentary 2
 
-	while ((i < myMessage.length) && (j < 300)) {
-		document.getElementById('hello_world').innerHTML += myMessage[i];
-		j++;
+	for (var letter of myMessage) {
+		text.innerHTML += letter; //mentioned in commentary 3
 	}
+
+  text.onclick = function play() { //mentioned in commentary 4
+  mySound.play();
+  }
 }
